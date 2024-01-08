d1203b241c2e81db184bb8afd564dc26256221ee
kaczka <manfil28@gmail.com>
2024-01-08 19:24:00 +0100

    Create spring project current dependecies:     -Spring Web     -Lombok     -Spring Security     -Mysql driver     -Spring JPA

---
+++ backend/TuneGuessr-server/src/main/java/com/example/tuneguessrserver/TuneGuessrServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuneguessrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneGuessrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuneGuessrServerApplication.class, args);
+    }
+
+}
---
+++ backend/TuneGuessr-server/src/test/java/com/example/tuneguessrserver/TuneGuessrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuneguessrserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneGuessrServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
