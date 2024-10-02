139a39ddcd8343c660e39056f35f5cbc37c26b02
kaczka <manfil28@gmail.com>
2024-10-02 12:59:15 +0200

    resolved conflict and changed BACKEND_URL from http to https

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,59 +1,66 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-import { SuccessPage } from './components/SuccessPage.jsx';
-import { LoginPage } from './views/LoginPage.jsx';
-import { RegisterPage } from './views/RegisterPage.jsx';
-import { ErrorPage } from './views/ErrorPage.jsx';
-import { SettingsPage } from './components/SettingsPage.jsx';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { SuccessPage } from "./components/SuccessPage.jsx";
+import { LoginPage } from "./views/LoginPage.jsx";
+import { RegisterPage } from "./views/RegisterPage.jsx";
+import { ErrorPage } from "./views/ErrorPage.jsx";
+import { SettingsPage } from "./components/SettingsPage.jsx";
+import { Play } from "./views/Play.jsx";
+import RoomPage from "./gameTest/room/RoomPage.jsx";
+import TestPage from "./gameTest/TestPage.jsx";
+import GamePage from "./gameTest/GamePage.jsx";
+import PickPage from "./gameTest/PickPage.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: '/',
-                element: <Play />,
-            },
-            {
-                path: '/settings',
-                element: <SettingsPage />,
-            },
-        ],
-    },
-	{
-		path: '/login',
-		element: <LoginPage />,
-	},
-	{
-		path: '/register',
-		element: <RegisterPage />,
-	},
-	{
-		path: '/success',
-		element: <SuccessPage />,
-	},
-    {
-        path: "/test",
-        element: <TestPage />,
-        children: [
-            {
-                path: "/test/game",
-                element: <GamePage />,
-            },
-            {
-                path: "/test/pick",
-                element: <PickPage />,
-            },
-            {
-                path: "/test/room",
-                element: <RoomPage />,
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Play />,
+      },
+      {
+        path: "/settings",
+        element: <SettingsPage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/success",
+    element: <SuccessPage />,
+  },
+  {
+    path: "/test",
+    element: <TestPage />,
+    children: [
+      {
+        path: "/test/game",
+        element: <GamePage />,
+      },
+      {
+        path: "/test/pick",
+        element: <PickPage />,
+      },
+      {
+        path: "/test/room",
+        element: <RoomPage />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
